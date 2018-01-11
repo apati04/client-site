58756d7b0f253f38b67d80e3ef188a9bc41fca84
Andrew Patipaksiri <apax714@gmail.com>
2018-01-10 16:54:41 -0800

    redux config

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,15 @@ import "semantic-ui-css/semantic.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>
+, document.getElementById('root'));
