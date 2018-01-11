e5acf1e3d9c64694f3e2495eb7dd15a134b65272
Andrew Patipaksiri <apax714@gmail.com>
2018-01-10 17:38:41 -0800

    test comment

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
-
+// redux store
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
