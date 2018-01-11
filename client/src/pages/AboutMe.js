12b7c502604b58f55dccae8a3f23f5351c7c39fe
isaac1104 <isaac-kwon@hotmail.com>
2018-01-10 22:22:18 -0800

    [design] Animation implemented

--- client/src/pages/AboutMe.js
+++ client/src/pages/AboutMe.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import SideBar from "./../components/SideBar";
-import { Image, Segment, Grid } from "semantic-ui-react";
+import { Image, Segment, Grid, Transition } from "semantic-ui-react";
 
 class AboutMe extends Component {
 
@@ -20,20 +20,22 @@ class AboutMe extends Component {
     return (
       <div style={style.container}>
         <SideBar/>
-        <Grid textAlign="center">
-          <Grid.Column width={4}>
-            <Image src="http://via.placeholder.com/350x150"/>
-          </Grid.Column>
-          <Grid.Column width={9}>
-            <Segment raised>
-              Bacon ipsum dolor amet tenderloin jowl ham venison bresaola pork loin drumstick shankle brisket pancetta. Bacon tail boudin pig turducken cupim pork chop buffalo strip steak shankle meatloaf. Burgdoggen prosciutto short ribs salami pork loin. Venison sirloin rump prosciutto ham hock drumstick ribeye shoulder pork t-bone corned beef beef filet mignon turducken.
+        <Transition animation="fade up" duration={2000} transitionOnMount={true}>
+          <Grid textAlign="center">
+            <Grid.Column width={4}>
+              <Image src="http://via.placeholder.com/350x150"/>
+            </Grid.Column>
+            <Grid.Column width={9}>
+              <Segment raised>
+                Bacon ipsum dolor amet tenderloin jowl ham venison bresaola pork loin drumstick shankle brisket pancetta. Bacon tail boudin pig turducken cupim pork chop buffalo strip steak shankle meatloaf. Burgdoggen prosciutto short ribs salami pork loin. Venison sirloin rump prosciutto ham hock drumstick ribeye shoulder pork t-bone corned beef beef filet mignon turducken.
 
-              Pork belly filet mignon frankfurter tongue, porchetta chuck beef shank andouille leberkas tenderloin flank ham tri-tip. Brisket fatback tenderloin chuck bresaola frankfurter venison shank meatloaf ribeye turkey buffalo pastrami kielbasa. Shoulder bacon jowl bresaola brisket pastrami. Filet mignon alcatra corned beef, ham hock tri-tip tail strip steak pork swine shoulder porchetta frankfurter ball tip rump capicola.
+                Pork belly filet mignon frankfurter tongue, porchetta chuck beef shank andouille leberkas tenderloin flank ham tri-tip. Brisket fatback tenderloin chuck bresaola frankfurter venison shank meatloaf ribeye turkey buffalo pastrami kielbasa. Shoulder bacon jowl bresaola brisket pastrami. Filet mignon alcatra corned beef, ham hock tri-tip tail strip steak pork swine shoulder porchetta frankfurter ball tip rump capicola.
 
-              Salami shank alcatra venison sausage short ribs shankle leberkas rump jowl pork ham hock shoulder pork loin. Ham hock brisket filet mignon beef ribs, porchetta tail swine jowl salami short loin picanha bresaola. Sirloin tongue venison ribeye shankle meatloaf. Pig porchetta pork loin, beef ribs chicken prosciutto t-bone jowl biltong kielbasa sausage tri-tip chuck swine. Tongue leberkas kevin, ribeye capicola alcatra rump ham hock biltong flank. Ham hock ball tip turducken leberkas, meatloaf frankfurter jerky andouille sausage shoulder rump drumstick meatball. Drumstick picanha spare ribs turducken capicola, pig shankle chicken landjaeger meatball filet mignon strip steak tenderloin.
-            </Segment>
-          </Grid.Column>
-        </Grid>
+                Salami shank alcatra venison sausage short ribs shankle leberkas rump jowl pork ham hock shoulder pork loin. Ham hock brisket filet mignon beef ribs, porchetta tail swine jowl salami short loin picanha bresaola. Sirloin tongue venison ribeye shankle meatloaf. Pig porchetta pork loin, beef ribs chicken prosciutto t-bone jowl biltong kielbasa sausage tri-tip chuck swine. Tongue leberkas kevin, ribeye capicola alcatra rump ham hock biltong flank. Ham hock ball tip turducken leberkas, meatloaf frankfurter jerky andouille sausage shoulder rump drumstick meatball. Drumstick picanha spare ribs turducken capicola, pig shankle chicken landjaeger meatball filet mignon strip steak tenderloin.
+              </Segment>
+            </Grid.Column>
+          </Grid>
+        </Transition>
       </div>
     );
   }
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,7 @@
 import React from "react";
 import background from "./../images/golf-course.jpg";
 import { Link } from "react-router-dom";
-import { Container, Header, Icon } from "semantic-ui-react";
+import { Container, Header, Icon, Transition } from "semantic-ui-react";
 
 const Landing = () => {
 
@@ -22,23 +22,25 @@ const Landing = () => {
   }
 
   return (
-    <Container
-      fluid
-      textAlign="center"
-      style={style.container}
-    >
-      <Container>
-        <Header size="huge" style={style.font}>JAMES OH</Header>
-        <Header size="large" style={style.font}>GOLF INSTRUCTOR</Header>
-        <Link to="/aboutme">
-          <Icon
-            name="arrow down"
-            size="huge"
-            color="orange"
-          />
-        </Link>
+      <Container
+        fluid
+        textAlign="center"
+        style={style.container}
+      >
+        <Transition animation="fade down" duration={3000} transitionOnMount={true}>
+          <Container>
+            <Header size="huge" style={style.font}>JAMES OH</Header>
+            <Header size="large" style={style.font}>GOLF INSTRUCTOR</Header>
+            <Link to="/aboutme">
+              <Icon
+                name="arrow down"
+                size="huge"
+                color="orange"
+              />
+            </Link>
+          </Container>
+        </Transition>
       </Container>
-    </Container>
   );
 }
 
